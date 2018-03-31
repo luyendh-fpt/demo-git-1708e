90e6a87c216fbb3caaf777c739a60fcde2e61dd9
Hong Luyen <luyendh@fpt.edu.vn>
2018-03-31 19:21:44 +0700

    Thêm contructor mới.

--- src/mypack/Account.java
+++ src/mypack/Account.java
@@ -14,6 +14,11 @@ public class Account {
     private int id;
     private String name;
 
+    public Account(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }        
+
     public int getId() {
         return id;
     }
