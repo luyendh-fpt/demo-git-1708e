6b226b45738c7a5a1671cc033d30cec77d02d1fb
Hong Luyen <luyendh@fpt.edu.vn>
2018-03-31 21:23:26 +0700

    Đã tách package theo yêu cầu.

--- src/model/AccountModel.java
+++ src/model/AccountModel.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mypack;
+package model;
 
+import entity.Account;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
