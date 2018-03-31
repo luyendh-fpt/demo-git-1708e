8997f146bd91f96d7d6e3eb585a168431839bad0
Hong Luyen <luyendh@fpt.edu.vn>
2018-03-31 19:27:53 +0700

    Add my function.

--- src/mypack/Account.java
+++ src/mypack/Account.java
@@ -13,6 +13,10 @@ public class Account {
 
     private int id;
     private String name;
+    
+    public void functionByDev1(){
+        System.out.println("Dev 1");
+    }
 
     public void getBalance(){
        System.out.println("Your balance");
